c22aac36445d55a5bb9ae73996c653ebbf80e53a
divta-suryawan <gitdivta@gmail.com>
2025-12-01 21:52:26 +0800

    create api and render gejala

--- resources/views/pages/gejala.blade.php
+++ resources/views/pages/gejala.blade.php
@@ -13,7 +13,7 @@
     >
         <div class="py-0">
             {{-- Table --}}
-            <x-base-table initId="gejalaTable" :columns="['No', 'Nama Gejala', 'Deskripsi', 'Aksi']">
+            <x-base-table initId="gejalaTable" :columns="['No', 'Kode Gejala', 'Kategori Gejala', 'Deskripsi', 'Aksi']">
                 <tbody>
 
                 </tbody>
@@ -33,17 +33,29 @@
         <input type="hidden" name="id" id="id" value="">
 
         <div class="form-group mb-3">
-            <label for="nama">Nama Gejala</label>
-            <input type="text" class="form-control" id="nama" name="nama"
-                placeholder="Masukkan nama gejala">
-            <small class="text-danger" id="nama-error"></small>
+            <label for="kode_gejala">Kode Gejala</label>
+            <input type="text" class="form-control" id="kode_gejala" name="kode_gejala"
+                placeholder="Contoh : G00x">
+            <small class="text-danger" id="kode_gejala-error"></small>
+        </div>
+        <div class="form-group mb-3">
+            <label for="ketagori">Kategori Gejala</label>
+            <select name="kategori" id="kategori" class="form-control">
+                <option value="" selected disabled>--pilih--</option>
+                <option value="daun">Daun</option>
+                <option value="buah">Buah</option>
+                <option value="batang">Batang</option>
+                <option value="akar">Akar</option>
+                <option value="umum">Umum</option>
+            </select>
+            <small class="text-danger" id="ketagori-error"></small>
         </div>
 
         <div class="form-group mb-3">
-            <label for="deskripsi">Deskripsi</label>
-            <textarea class="form-control" id="deskripsi" name="deskripsi"
+            <label for="deskripsi_gejala">Deskripsi</label>
+            <textarea class="form-control" id="deskripsi_gejala" name="deskripsi_gejala"
                 placeholder="Masukkan deskripsi gejala"></textarea>
-            <small class="text-danger" id="deskripsi-error"></small>
+            <small class="text-danger" id="deskripsi_gejala-error"></small>
         </div>
 
     </x-base-form>
